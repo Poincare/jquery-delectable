7ef8f46e18130ce27ee27f439e4338998f48e9eb
Dhaivat Pandya <dhaivatpandya@gmail.com>
2011-11-25 14:49:46 -0600

    Initial commit

---
+++ jquery-delectable.js
@@ -0,0 +1,9 @@
+(function ( $ ) {
+    $.fn.delectable = function() {
+        $(this).click(function() {
+            $(this)[0].focus();
+            $(this)[0].select();
+        });
+    }
+    
+})( jQuery )
